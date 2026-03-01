300f2333cf9470c9a77c7b933a0c6426d2c8c280
rudytyms <rudytyms@gmail.com>
2026-03-01 16:08:37 +0100

    and another fix

--- Source/Entities/Enemies/enemy.gd
+++ Source/Entities/Enemies/enemy.gd
@@ -1,14 +1,11 @@
 # the skorpogest
 # Znane bugi: 0
-
 extends CharacterBody2D
 class_name Enemy
-
 
 # =========================
 # ZMIENNE
 # =========================
-
 @export var max_hp: int = 120
 @export var speed := 260.0
 @export var turn_speed := 8.0
@@ -16,33 +13,40 @@ class_name Enemy
 @onready var agent: NavigationAgent2D = $EnemyNavigation
 
 var hp: int
-var player: Node2D
-var move_dir := Vector2.RIGHT
+var player: Node2D = null
+var move_dir: Vector2 = Vector2.RIGHT
 var dead := false
+<<<<<<< HEAD
 var frozen := false
+=======
+
+>>>>>>> 6b032a27bbdb579a0ded68bcbc75c3dac411f84f
 var _saved_collision_layer: int
 var _saved_collision_mask: int
 
-
 # =========================
-# FUNKCJE WBUDOWANE
+# READY
 # =========================
-
 func _ready() -> void:
 	add_to_group("enemy")
 	hp = max_hp
-
-	player = get_tree().get_first_node_in_group("player")
-	if player == null:
-		push_error("the whatest")
-		set_physics_process(false)
-		return
-
 	move_dir = Vector2.RIGHT.rotated(randf() * TAU)
 
+	_find_player_async()
 
+func _find_player_async() -> void:
+	while player == null:
+		player = get_tree().get_first_node_in_group("player")
+		await get_tree().process_frame
+
+# =========================
+# PHYSICS
+# =========================
 func _physics_process(delta: float) -> void:
 	if dead or frozen:
+		return
+
+	if player == null:
 		return
 
 	agent.target_position = player.global_position
@@ -52,21 +56,18 @@ func _physics_process(delta: float) -> void:
 		move_and_slide()
 		return
 
-	var next_point := agent.get_next_path_position()
-	var desired_dir := (next_point - global_position).normalized()
+	var next_point: Vector2 = agent.get_next_path_position()
+	var desired_dir: Vector2 = (next_point - global_position).normalized()
 
 	move_dir = move_dir.slerp(desired_dir, turn_speed * delta).normalized()
 	velocity = move_dir * speed
-
 	move_and_slide()
 
 	rotation = move_dir.angle()
 
-
 # =========================
 # LOGIKA GRY
 # =========================
-
 func set_collision_enabled(value: bool) -> void:
 	if value:
 		collision_layer = _saved_collision_layer
@@ -77,7 +78,6 @@ func set_collision_enabled(value: bool) -> void:
 		collision_layer = 0
 		collision_mask = 0
 
-
 func take_damage(amount: int, _hit_pause := 0.0) -> void:
 	if dead:
 		return
@@ -87,7 +87,6 @@ func take_damage(amount: int, _hit_pause := 0.0) -> void:
 
 	if hp <= 0:
 		die()
-
 
 func die() -> void:
 	dead = true
