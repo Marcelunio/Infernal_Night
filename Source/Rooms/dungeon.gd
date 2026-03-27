d3d6c079338527bf84643571cbe913712c6aabb1
rudytyms <rudytyms@gmail.com>
2026-03-27 20:20:43 +0100

    fix v3

--- Source/Rooms/dungeon.gd
+++ Source/Rooms/dungeon.gd
@@ -196,7 +196,7 @@ func setup_room_doors(room: Node2D, pos: Vector2i):
 
 func spawn_player():
 	player = PLAYER.instantiate()
-	player.vanTilemap = get_node("Room/NavigationRegion2D/LayerVisible")#Kleks
+	player.vanTilemap = get_node("Room/NavigationRegion2D/RoomLayout")#Kleks
 	add_child(player)
 	player.position = ROOM_SIZE/2
 	current_room_pos = Vector2i.ZERO
